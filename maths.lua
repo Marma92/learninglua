15e6bb719cfc2c5400586e24f479611b4c48ed63
Amram ELBAZ <elbaz.amram@gmail.com>
2024-12-24 14:28:06 +0100

    thought of the edge case of exp 0

--- maths.lua
+++ maths.lua
@@ -1,59 +1,64 @@
 --[[ 
     Now let's do some basic maths operations ! 
-]]--
+]]
+--
 
 function add(var1, var2)
-    if (type(var1) ~= 'number' or type(var2) ~= 'number') then
-        return error("NaN", 1)
-    end
-    return var1 + var2
+	if type(var1) ~= "number" or type(var2) ~= "number" then
+		return error("NaN", 1)
+	end
+	return var1 + var2
 end
 
 function sub(var1, var2)
-    if (type(var1) ~= 'number' or type(var2) ~= 'number') then
-        return error("NaN", 1)
-    end
-    return var1 - var2
+	if type(var1) ~= "number" or type(var2) ~= "number" then
+		return error("NaN", 1)
+	end
+	return var1 - var2
 end
 
 function div(var1, var2)
-    if (type(var1) ~= 'number' or type(var2) ~= 'number') then
-        return error("NaN", 1)
-    end
-    return var1 / var2
+	if type(var1) ~= "number" or type(var2) ~= "number" then
+		return error("NaN", 1)
+	end
+	return var1 / var2
 end
 
 function mult(var1, var2)
-    if (type(var1) ~= 'number' or type(var2) ~= 'number') then
-        return error("NaN", 1)
-    end
-    return var1 * var2
+	if type(var1) ~= "number" or type(var2) ~= "number" then
+		return error("NaN", 1)
+	end
+	return var1 * var2
 end
 
 --[[ 
     computing exponent
-]]--
+]]
+--
 function exp(var1, var2)
-    if (type(var1) ~= 'number' or type(var2) ~= 'number') then
-        return error("NaN", 1)
-    end
-    local result = var1
-    for i = 1, var2, 1 do
-        result = result * var1
-    end
-    return result
+	if type(var1) ~= "number" or type(var2) ~= "number" then
+		return error("NaN", 1)
+	end
+	if var1 == 0 then
+		return 1
+	end
+	local result = var1
+	for i = 1, var2, 1 do
+		result = result * var1
+	end
+	return result
 end
 
 --[[ 
     percentile
-]]--
+]]
+--
 function perc(var1, var2)
-    if (type(var1) ~= 'number' or type(var2) ~= 'number') then
-        return error("NaN", 1)
-    end
-    return var1 *100 / var2.."%"
+	if type(var1) ~= "number" or type(var2) ~= "number" then
+		return error("NaN", 1)
+	end
+	return var1 * 100 / var2 .. "%"
 end
-
 
 print(add(10, 100))
 print(sub(10, 100))
